0f919b718bad082ac0b8a6fd6903a1903a18ed53
erisonn <erison.frontend@gmail.com>
2024-07-16 12:33:26 -0300

    rendering router and app header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Router";
+import AppHeader from "./components/AppHeader";
+
 function App() {
-  console.log(import.meta.env.VITE_APY_KEY);
-  return <>HELLO WORLD!</>;
+  // console.log(import.meta.env.VITE_APY_KEY);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <AppHeader />
+        <Router />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
