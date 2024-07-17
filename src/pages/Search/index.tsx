c81835790f4770b884c14bc2b8384faf0fb04a6f
erisonn <erison.frontend@gmail.com>
2024-07-17 12:38:45 -0300

    test react query request

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,4 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchFunction } from "../../clientconfig";
+
 const Search = () => {
+  const query = useQuery({ queryKey: ["movies"], queryFn: fetchFunction });
+  const { Search: searchResults, totalResults } = query.data ?? {};
+
+  console.log(searchResults, totalResults);
   return <h1>Search for movies here!</h1>;
 };
 
