c6dd4ffe559b8af0f32f9b6ace2ebfec63b1246b
erisonn <erison.frontend@gmail.com>
2024-07-22 11:42:03 -0300

    add default value to page number param

--- src/clientconfig.ts
+++ src/clientconfig.ts
@@ -3,7 +3,10 @@ import { QueryClient } from "@tanstack/react-query";
 const API_KEY = import.meta.env.VITE_API_KEY;
 const baseUrl = `http://www.omdbapi.com/?apikey=${API_KEY}&page=`;
 
-export const fetchFunction = async (pageParam: number, queryString: string ) => {
+export const fetchFunction = async (
+  pageParam: number = 1,
+  queryString: string
+) => {
   const results = await fetch(baseUrl + pageParam + queryString);
   return results.json();
 };
