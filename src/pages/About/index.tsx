2e634871eb0791dbacb72ccc76fe10ffab0c2af2
erisonn <erison.frontend@gmail.com>
2024-07-22 13:12:15 -0300

    about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,5 +1,13 @@
+import "./index.scss";
+
 const About = () => {
-  return <h1>About here!</h1>;
+  return (
+    <div className="About Centered">
+      <h1>The Open Movie Database</h1>
+      <img src={"/about.jpg"} alt="img" />
+      <p>'Blade Runner 2049' International Poster</p>
+    </div>
+  );
 };
 
 export default About;
