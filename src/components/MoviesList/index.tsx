82499aa2388ead07b88f1fa0c8e59c3fe88fad94
erisonn <erison.frontend@gmail.com>
2024-07-30 13:42:47 -0300

    using moviecard component

--- src/components/MoviesList/index.tsx
+++ src/components/MoviesList/index.tsx
@@ -1,6 +1,7 @@
 import { useIntersectionObserver } from "usehooks-ts";
 import { useEffect } from "react";
 import "./index.scss";
+import MovieCard from "../MovieCard";
 
 export type Movie = {
   Title: string;
@@ -34,14 +35,9 @@ const MoviesList = ({
     <div className="MoviesList">
       {movies.map((movie) => (
         <div
-          className="MoviesListItem"
-          key={movie?.imdbID}
           ref={movie?.imdbID === lastItem?.imdbID ? infiniteScrollRef : null}
         >
-          <img src={movie?.Poster} />
-          <h3>
-            {movie?.Title} - {movie.Year}
-          </h3>
+          <MovieCard movie={movie} />
         </div>
       ))}
     </div>
