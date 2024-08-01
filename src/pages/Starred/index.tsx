310900003a7dcd50430efb2669c8c6821c776e55
erisonn <erison.frontend@gmail.com>
2024-07-31 23:15:29 -0300

    listing movies from localstorage

--- src/pages/Starred/index.tsx
+++ src/pages/Starred/index.tsx
@@ -1,5 +1,19 @@
+import MoviesList, { Movie } from "../../components/MoviesList";
+import './index.scss'
+
 const Starred = () => {
-  return <h1>Starred movies here!</h1>;
+  const movies: Movie[] = JSON.parse(
+    localStorage.getItem("starredMovies") ?? "[]"
+  );
+
+  return (
+    <div className="Centered">
+      <div className="Starred">
+        <h1>Your starred movies</h1>
+        <MoviesList movies={movies} loadMore={() => {}} />
+      </div>
+    </div>
+  );
 };
 
 export default Starred;
