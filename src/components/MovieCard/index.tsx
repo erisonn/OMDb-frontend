cbbf756dcabf4c7e75ef1250ab9759f14a3c9d9b
erisonn <erison.frontend@gmail.com>
2024-07-31 23:15:20 -0300

    add function to save movies on localstorage

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -2,8 +2,27 @@ import { Movie } from "../MoviesList";
 import "./index.scss";
 
 const MovieCard = ({ movie }: { movie: Movie }) => {
+  const favoriteMovie = (movie: Movie) => {
+    const moviesFromLocalStorage =
+      JSON.parse(localStorage.getItem("starredMovies") ?? "[]") ?? [];
+
+    const isMovieAlreadyStarred = !!moviesFromLocalStorage.find(
+      (lsMovie: Movie) => lsMovie.imdbID === movie.imdbID
+    );
+    if (isMovieAlreadyStarred) return;
+
+    const newMoviesFromLocalStorage = JSON.stringify([
+      ...moviesFromLocalStorage,
+      movie,
+    ]);
+    localStorage.setItem("starredMovies", newMoviesFromLocalStorage);
+  };
+
   return (
     <div className="MovieCard" key={movie?.imdbID}>
+      <button className="StarButton" onClick={() => favoriteMovie(movie)}>
+        ☆
+      </button>
       <img src={movie?.Poster} />
       <h3>
         {movie?.Title} - {movie.Year}
